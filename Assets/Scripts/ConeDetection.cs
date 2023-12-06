22f7f2ed577cfb37f664e026ee48b8781653dc69
Dorisig <halldors22@ru.is>
2023-12-06 19:02:23 +0000

    addded pickup statues

--- Assets/Scripts/ConeDetection.cs
+++ Assets/Scripts/ConeDetection.cs
@@ -1,14 +1,29 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI; // Needed for UI elements
+using TMPro;
+
 
 public class ConeDetection : MonoBehaviour
 {
 
     public Material highlightMaterial; // Assign this in the Inspector
     private Material normalMaterial; // Store the original material
+    public TextMeshProUGUI statueCountText; // Change to TextMeshProUGUI
+    private int statueCount = 0; // To keep track of statues interacted with
+    private GameObject currentStatue = null; // To keep track of the current statue
 
 
+
+    void Update()
+    {
+        // Check if "E" is pressed and there's a current statue
+        if (Input.GetKeyDown(KeyCode.E) && currentStatue != null)
+        {
+            InteractWithStatue();
+        }
+    }
     void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.CompareTag("Flask"))
@@ -18,6 +33,7 @@ public class ConeDetection : MonoBehaviour
         if (other.gameObject.CompareTag("Statue"))
         {
             HighlightObject(other.gameObject);
+            currentStatue = other.gameObject;
         }
         // Add more conditions here for other interactable objects
     }
@@ -31,6 +47,7 @@ public class ConeDetection : MonoBehaviour
         if (other.gameObject.CompareTag("Statue"))
         {
             RemoveHighlight(other.gameObject);
+            currentStatue = null;
         }
         // Add more conditions here for other interactable objects
     }
@@ -56,5 +73,15 @@ public class ConeDetection : MonoBehaviour
         {
             renderer.material = normalMaterial;
         }
+    }
+
+    void InteractWithStatue()
+    {
+        statueCount++; // Increase statue count
+        statueCountText.text = statueCount.ToString() + "/2"; // Update TMP text
+
+        // Destroy the statue object
+        Destroy(currentStatue);
+        currentStatue = null; // Reset current statue
     }
 }
