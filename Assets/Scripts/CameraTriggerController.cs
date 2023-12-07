0b7dd48bf2f327244dc1fdedbe912352eabfe3b7
Bjarki Rafn Þórðarson <bjarki@thordarson.is>
2023-12-07 15:56:37 +0000

    Camera rigging

--- Assets/Scripts/CameraTriggerController.cs
+++ Assets/Scripts/CameraTriggerController.cs
@@ -12,4 +12,21 @@ public class CameraTriggerController : MonoBehaviour
             CameraHandler.instance.SetActiveCamera(camera);
         }
     }
+
+    private void OnDrawGizmos()
+    {
+        BoxCollider collider = GetComponent<BoxCollider>();
+        if (collider == null)
+            return;
+
+        Gizmos.matrix = Matrix4x4.TRS(transform.position, transform.rotation, transform.localScale);
+
+        // Draw opaque yellow box
+        Gizmos.color = new Color(1, 1, 0, 0.5f);
+        Gizmos.DrawCube(collider.center, collider.size);
+
+        // Draw red wireframe
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireCube(collider.center, collider.size);
+    }
 }
