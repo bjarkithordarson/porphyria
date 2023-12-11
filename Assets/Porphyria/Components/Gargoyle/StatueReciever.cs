7821d4355073c88ab6b98cc3862987fe91992d8b
DESKTOP-HRN87SN\dadir <dadirussel@gmail.com>
2023-12-11 23:13:32 +0000

    StatueReciever work in progress

--- Assets/Porphyria/Components/Gargoyle/StatueReciever.cs
+++ Assets/Porphyria/Components/Gargoyle/StatueReciever.cs
@@ -1,18 +1,18 @@
 using System.Collections;
 using TMPro;
-using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.SceneManagement;
-
-
 public class StatueReciever : MonoBehaviour
 {
 public int statuesNeeded;
-private GameManager gameManager;
+//private GameManager GameManager;
 public TextMeshProUGUI depositText;
 public SceneManager SceneManager;
 public GameObject Flooring;
+public GameObject CounterWeight;
+public GameObject Fire;
 private Animator Railing_Pillar_Low_01;
+// private ConeDetection ConeDetection;
 
 private bool canReturnStatues = false;
 
@@ -45,11 +45,11 @@ private bool canReturnStatues = false;
     }
     IEnumerator AllStatuesPlaced()
     {
-        if(gameManager.AmountofStatuesNeeded < gameManager.AmountOfPlacedStatues){
+        // if(GameManager.instance.AmountofStatuesNeeded < GameManager.instance.AmountOfPlacedStatues){
         yield return new WaitForSeconds(3.5f);
         depositText.text = "All statues placed";
         depositText.enabled = true;
-        }
+        // }
         // Add code to place the statues here
         //StartCoroutine(LoadSecretStudyScreen());
         
@@ -68,8 +68,17 @@ private bool canReturnStatues = false;
         {
             depositText.text = "Placing statue...";
             depositText.enabled = true;
-            Railing_Pillar_Low_01.SetBool("PillarAnimation",true);
-            //StartCoroutine(AllStatuesPlaced());           
+            ConeDetection.instance.statueCount = 0;
+            GameManager.instance.AmountOfPlacedStatues++;
+            CounterWeight.SetActive(true);
+            Fire.SetActive(true);
+            Railing_Pillar_Low_01.enabled = true;
+            
+            if(GameManager.instance.AmountOfPlacedStatues == GameManager.instance.AmountofStatuesNeeded)
+            {
+                StartCoroutine(AllStatuesPlaced());
+            }
+                      
 
         }
     }
