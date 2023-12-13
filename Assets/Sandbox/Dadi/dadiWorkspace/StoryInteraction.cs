a441ebc2f17b9f7c7fe79956d0b706a7e4787ff4
SpicyBeard <dadirussel@gmail.com>
2023-12-13 12:55:46 +0000

    starting up the story interactebles

---
+++ Assets/Sandbox/Dadi/dadiWorkspace/StoryInteraction.cs
@@ -0,0 +1,22 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StoryInteraction : MonoBehaviour
+{
+    public GameObject Light;
+    public Boolean IsItBook;
+    public 
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
