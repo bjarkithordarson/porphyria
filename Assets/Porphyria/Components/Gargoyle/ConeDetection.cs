65b39653fc9c5b556d047d97c06376c2a5195fef
Dorisig <halldors22@ru.is>
2023-12-14 18:53:33 +0000

    Added MainGameLevel under scenes, added the StatuePillarMechanic into mainmap and added juice, rewrote ConeDetection, added StatueScript, modifyied statuereicieaivierer.

--- Assets/Porphyria/Components/Gargoyle/ConeDetection.cs
+++ Assets/Porphyria/Components/Gargoyle/ConeDetection.cs
@@ -1,79 +1,57 @@
-
 using UnityEngine;
 using TMPro;
 
-
 public class ConeDetection : MonoBehaviour
 {
-    public static ConeDetection instance;
+    public int statueCount = 0;
+    public int requiredStatueCount = 1;
+    public TextMeshProUGUI statueCountText;
 
-    public TextMeshProUGUI statueCountText; // Change to TextMeshProUGUI
-    public int statueCount = 0; // To keep track of statues interacted with
-    public int requiredStatueCount;
-    private GameObject currentStatue = null; // To keep track of the current statue
-    public TextMeshPro interactionText;
+    private GameObject currentStatue = null;
+    public static ConeDetection instance; // Singleton instance
 
-        void Awake()
+    
+    public void ResetStatueCount()
     {
-        instance = this;
+        statueCount = 0;
+        statueCountText.text = "";
     }
-
-
-    void Update()
+    
+    private void Update()
     {
-        // Check if "E" is pressed and there's a current statue
         if (Input.GetKeyDown(KeyCode.E) && currentStatue != null)
         {
-            if(statueCount != requiredStatueCount)
+            if(statueCount == 0){
+
+            
+            StatueScript statueScript = currentStatue.GetComponent<StatueScript>();
+            if (statueScript != null)
             {
-            InteractWithStatue();
-            }
-        }
-        if(statueCount == 0)
-        {
-            //ResetStatueCount();
+                statueCount++;
+                statueCountText.text = statueCount.ToString() + "/" + requiredStatueCount;
+                statueScript.Interact();
+                Destroy(currentStatue);
+                currentStatue = null;
+            }}
         }
     }
-    void OnTriggerEnter(Collider other)
+
+    private void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.CompareTag("Statue"))
         {
             currentStatue = other.gameObject;
-            if (statueCount == requiredStatueCount)
-            {
-                interactionText.text = " You have enough statues ";
-            }
-            interactionText.gameObject.SetActive(true);
         }
-        // Add more conditions here for other interactable objects
     }
 
-    public void ResetStatueCount()
+    private void OnTriggerExit(Collider other)
     {
-        statueCount = 0;
-        interactionText.text = "";
-    }
-    void OnTriggerExit(Collider other)
-    {
-        if (other.gameObject.CompareTag("Statue"))
+        if (other.gameObject == currentStatue)
         {
             currentStatue = null;
-            interactionText.text = " Pickup statue ";
-            interactionText.gameObject.SetActive(false);
         }
-        // Add more conditions here for other interactable objects
-    }
-
-void InteractWithStatue()
-{   
-
-        statueCount++; // Increase statue count
-        statueCountText.text = statueCount.ToString() + "/" + requiredStatueCount; // Update TMP text
-
-        Destroy(currentStatue); // Destroy the statue object
-        currentStatue = null; // Reset current statue
-        interactionText.gameObject.SetActive(false); // Disable interaction text
     }
 }
+
 
 
